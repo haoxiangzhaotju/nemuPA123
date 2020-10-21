b4682a1016ae91d3eef91187fdd084422d9e6ecc
tracer-ics2016 <tracer@njuics.org>
2020-10-21 13:48:20 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:48:20 up 9 days,  7:27,  8 users,  load average: 1.10, 0.68, 0.29 4d4c7d543b4ca1cc5d376a31b00e30d02734d76f

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -1,12 +1,21 @@
 #include "cpu/exec/template-start.h"
 
 #define instr call
-
-static void do_execute(){
+/*static void do_execute(){
 	OPERAND_W(op_dest,op_src->val);
 	print_asm_template2();
+}*/
+make_helper (concat(call_i_, SUFFIX))
+{
+	int len = concat(decode_i_, SUFFIX) (eip + 1);
+	reg_l (R_ESP) -= DATA_BYTE;
+	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",cpu.eip + 1 + len + displacement);
+	cpu.eip +=displacement;
+	return len + 1;
 }
 
-make_instr_helper(i);
+//make_instr_helper(i);
 #include "cpu/exec/template-end.h"
 //rtl_subi
