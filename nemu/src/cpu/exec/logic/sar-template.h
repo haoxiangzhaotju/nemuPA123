65bb02602125c5480c4b4b8d462e8c2a8cb06c00
tracer-ics2016 <tracer@njuics.org>
2020-10-29 16:47:29 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:47:29 up 17 days, 10:26, 13 users,  load average: 0.47, 0.66, 0.51 407064489d257d74d89db21792d5ed4fc2483aa9

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -5,14 +5,20 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
-
+	DATA_TYPE_S result = op_dest->val;
 	uint8_t count = src & 0x1f;
 	dest >>= count;
-	OPERAND_W(op_dest, dest);
-
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	result = dest;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF=0;
+	cpu.OF=0;
+	cpu.SF=result >> len;
+    	cpu.ZF=!result;
+    	OPERAND_W(op_dest, dest);
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
 	print_asm_template2();
 }
 
