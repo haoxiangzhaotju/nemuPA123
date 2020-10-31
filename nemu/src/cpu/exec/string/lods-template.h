3d376cf27bab7a134e5da6be1716267d5123b3b7
tracer-ics2016 <tracer@njuics.org>
2020-10-31 14:44:21 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:44:21 up 1 day,  1:18, 10 users,  load average: 0.25, 0.16, 0.10 a094f8a4d7f2d1bae3e01d39c34a0702ee27a5bb

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -6,13 +6,13 @@ static void do_execute(){
 //	DATA_TYPE src;
 	if(ops_decoded.is_operand_size_16){
 		//src = swaddr_read(reg_w(R_SI),DATA_BYTE);
-		swaddr_write(REG(R_AX),2,swaddr_read(reg_w(R_SI),4));
+		swaddr_write(reg_w(R_AX),2,swaddr_read(reg_w(R_SI),4));
 		if(cpu.DF==0)reg_w(R_SI)+=DATA_BYTE;
 		else reg_l(R_SI)-=DATA_BYTE;
 	}
 	else{
 		//src=swaddr_read(reg_l(R_ESI),DATA_BYTE);
-		swaddr_write(REG(R_EAX),4,swaddr_read(reg_l(R_ESI),4));
+		swaddr_write(reg_l(R_EAX),4,swaddr_read(reg_l(R_ESI),4));
 		if(cpu.DF==0)reg_l(R_ESI)+=DATA_BYTE;
 		else reg_l(R_ESI)-=DATA_BYTE;
 	}
