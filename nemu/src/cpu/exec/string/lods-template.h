932749a0255c638bfedc33f3ac2fa10e94867b96
tracer-ics2016 <tracer@njuics.org>
2020-10-31 14:35:36 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:35:36 up 1 day,  1:10, 10 users,  load average: 0.07, 0.06, 0.07 e58efda23fa2a9c04ed045cb8ab163341d281fa5

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -6,7 +6,7 @@ static void do_execute(){
 //	DATA_TYPE src;
 	if(ops_decoded.is_operand_size_16){
 		//src = swaddr_read(reg_w(R_SI),DATA_BYTE);
-		swaddr_write(reg_w(R_AX),2,swaddr_read(reg_w(R_SI),4));
+		swaddr_write(REG(R_AX),2,swaddr_read(reg_w(R_SI),4));
 		if(cpu.DF==0)REG(R_EDI)+=DATA_BYTE,
 		REG(R_ESI)+=DATA_BYTE;
 		else REG(R_EDI)-=DATA_BYTE,
@@ -14,7 +14,7 @@ static void do_execute(){
 	}
 	else{
 		//src=swaddr_read(reg_l(R_ESI),DATA_BYTE);
-		swaddr_write(reg_l(R_EAX),4,swaddr_read(reg_l(R_ESI),4));
+		swaddr_write(REG(R_EAX),4,swaddr_read(reg_l(R_ESI),4));
 		if(cpu.DF==0)REG(R_EDI)+=DATA_BYTE,
 		REG(R_ESI)+=DATA_BYTE;
 		else REG(R_EDI)-=DATA_BYTE,
