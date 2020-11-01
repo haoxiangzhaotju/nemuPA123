78ffb0f81e1e12e6b566b15693c737aecc432900
tracer-ics2016 <tracer@njuics.org>
2020-11-01 17:58:29 +0000

    >  run 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:58:29 up 2 days,  4:32,  7 users,  load average: 0.89, 0.64, 0.48 1d396a663c64d847268394e6950c2063f0643cf7

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -7,46 +7,77 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
 	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
-
-	nemu_assert(0);
-	return 0;
+	* `libgcc', other than newlib. It is a dirty work to port `libgcc'
+	* to NEMU. In fact, it is unnecessary to perform a "64/64" division
+	* here. A "64/32" division is enough.
+	*
+	* To perform a "64/32" division, you can use the x86 instruction
+	* `div' or `idiv' by inline assembly. We provide a template for you
+	* to prevent you from uncessary details.
+	*
+	*     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
+	*
+	* If you want to use the template above, you should fill the "???"
+	* correctly. For more information, please read the i386 manual for
+	* division instructions, and search the Internet about "inline assembly".
+	* It is OK not to use the template above, but you should figure
+	* out another way to perform the division.
+	*/
+	int sign = 1;
+	if (a < 0)
+	{
+		sign = -sign;
+		a = -a;
+	}
+	if (b < 0)
+	{
+		sign = -sign;
+		b = -b;
+	}
+	int result = a/b;
+	a = a%b;
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		a <<= 1;
+		result <<= 1;
+		if (a >= b)
+		{
+			a -= b;
+			++result;
+		}
+		
+	}
+	
+	return result*sign;
 }
-
 FLOAT f2F(float a) {
 	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
-
-	nemu_assert(0);
-	return 0;
+	* introducing x87 floating point instructions. Else you can
+	* not run this code in NEMU before implementing x87 floating
+	* point instructions, which is contrary to our expectation.
+	*
+	* Hint: The bit representation of `a' is already on the
+	* stack. How do you retrieve it to another variable without
+	* performing arithmetic operations on it directly?
+	*/
+	
+	int b = *(int *)&a;
+	int sign = b >> 31;
+	int exp = (b >> 23) & 0xff;
+	FLOAT k = b & 0x7fffff;
+	if (exp != 0) k += 1<< 23;
+	exp -= 150;
+	if (exp < -16) k >>= -16-exp;
+	if (exp > -16) k <<= 16+exp;
+	return sign ? -k : k;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	
+	return a < 0 ? -a : a;
 }
+
 
 /* Functions below are already implemented */
 
