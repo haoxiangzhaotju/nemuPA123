0d42106f5557d5e8c848d4dacd58c537df5dde7e
tracer-ics2016 <tracer@njuics.org>
2020-11-01 13:36:38 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:36:38 up 2 days, 11 min,  9 users,  load average: 0.24, 0.25, 0.24 950da86758aafb2831e903e328a991f6605ac1cc

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -2,23 +2,15 @@
 
 #define instr stos
 
-static void do_execute () {
-	if (ops_decoded.is_operand_size_16)
-	{
-		swaddr_write (reg_w(R_DI),2,reg_w(R_AX));
-		if (cpu.DF == 0)reg_w (R_DI) += DATA_BYTE;
-		else reg_w (R_DI) -= DATA_BYTE;
-	}
+make_helper(concat(stos_n_,SUFFIX))
+{
+	MEM_W(reg_l(R_EDI),REG(R_EAX));
+	if(cpu.DF==0)
+		reg_l(R_EDI)+=DATA_BYTE;
 	else
-	{
-		swaddr_write (reg_l(R_EDI),4,reg_l(R_EAX));
-		if (cpu.DF == 0)reg_l (R_EDI) += DATA_BYTE;
-		else reg_l (R_EDI) -= DATA_BYTE;
-	}
-	print_asm("stos");
+		reg_l(R_EDI)-=DATA_BYTE;
+	print_asm("stos%s",str(SUFFIX));
+	return 1;
 }
-
-make_instr_helper(n)
-
 
 #include "cpu/exec/template-end.h"
