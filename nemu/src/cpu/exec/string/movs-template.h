82811e4301f1d03419b5da58f941fdc74aaea22e
tracer-ics2016 <tracer@njuics.org>
2020-11-01 13:13:10 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:13:10 up 1 day, 23:47,  9 users,  load average: 0.30, 0.27, 0.30 cce6846b6f59fde9ac9d439c86b1eb369c105211

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -2,30 +2,16 @@
 
 #define instr movs
 
-static void do_execute () {
-	if (ops_decoded.is_operand_size_16){
-		swaddr_write (reg_w(R_DI),2,swaddr_read (reg_w(R_SI),4));
-		if (cpu.DF == 0){
-			reg_w (R_DI) += DATA_BYTE;
-			reg_w (R_SI) += DATA_BYTE;
-		}
-		else{
-			reg_w (R_DI) -= DATA_BYTE;
-			reg_w (R_SI) -= DATA_BYTE;
-		}
-	}
-	else{
-		swaddr_write (reg_l(R_EDI),4,swaddr_read (reg_l(R_ESI),4));
-		if (cpu.DF == 0){
-			reg_l (R_EDI) += DATA_BYTE;
-			reg_l (R_ESI) += DATA_BYTE;
-		}
-		else{
-			reg_l (R_EDI) -= DATA_BYTE;
-			reg_l (R_ESI) -= DATA_BYTE;
-		}
-	}
+make_helper(concat(movs_n_,SUFFIX))
+{
+	swaddr_write(reg_l(R_EDI),DATA_BYTE,swaddr_read(reg_l(R_ESI),DATA_BYTE));
+	if(cpu.DF==0)
+		reg_l(R_EDI)+=DATA_BYTE,
+		reg_l(R_ESI)+=DATA_BYTE;
+	else
+		reg_l(R_EDI)-=DATA_BYTE,
+		reg_l(R_ESI)-=DATA_BYTE;
 	print_asm("movs");
-}
-make_instr_helper(n)
+	return 1;
+}	
 #include "cpu/exec/template-end.h"
