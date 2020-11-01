8adfeaef5742d40d7ba55c60637e0bc1f0db125c
tracer-ics2016 <tracer@njuics.org>
2020-11-01 03:42:24 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:42:24 up 1 day, 14:16,  9 users,  load average: 0.31, 0.21, 0.09 d26fd5109d53fe8316099e4f1f724b6d86c647f

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -2,23 +2,15 @@
 
 #define instr lods
 
-static void do_execute(){
+make_helper(concat(lods_n_,SUFFIX)){
 //	DATA_TYPE src;
-	if(ops_decoded.is_operand_size_16){
-		//src = swaddr_read(reg_w(R_SI),DATA_BYTE);
-		swaddr_write(reg_w(R_AX),2,swaddr_read(reg_w(R_SI),4));
-		if(cpu.DF==0)reg_w(R_SI)+=DATA_BYTE;
-		else reg_l(R_SI)-=DATA_BYTE;
-	}
-	else{
-		//src=swaddr_read(reg_l(R_ESI),DATA_BYTE);
-		swaddr_write(reg_l(R_EAX),4,swaddr_read(reg_l(R_ESI),4));
-		if(cpu.DF==0)reg_l(R_ESI)+=DATA_BYTE;
-		else reg_l(R_ESI)-=DATA_BYTE;
-	}
-	print_asm("lods");
+	REG(R_EAX) = swaddr_read(reg_l(R_ESI),DATA_BYTE);
+	if(cpu.DF==0)reg_l(R_ESI)+=DATA_BYTE;
+	else reg_l(R_ESI)-=DATA_BYTE;
+	print_asm("lods%s",str(SUFFIX));
+	return 1;
+	
 }
 
-make_instr_helper(n)
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -55,10 +55,12 @@ make_helper(repnz) {
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
 			  );
-
-			if ( cpu.ZF == 1||cpu.ecx==0)break;
 		/* TODO: Jump out of the while loop if necessary. */
 
+			if ((ops_decoded.opcode == 0xa6	
+			|| ops_decoded.opcode == 0xa7	
+			|| ops_decoded.opcode == 0xae
+			|| ops_decoded.opcode == 0xaf) && cpu.ZF == 0)break;
 	}
 
 #ifdef DEBUG
