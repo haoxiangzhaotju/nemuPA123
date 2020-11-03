f3437a810ab6d5baa2d22192c41205c600f227fa
tracer-ics2016 <tracer@njuics.org>
2020-11-03 16:37:55 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:37:55 up 4 days,  3:12,  4 users,  load average: 0.40, 0.49, 0.50 c32616e6ac7b5be3782bac552eaa120488b0833

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -269,11 +269,11 @@ uint32_t expr(char *e, bool *success){
 	}
 	int i;
 	for (i = 0;i < nr_token; i ++) {
- 		if (tokens[i].type=='*'&&(i==0||(tokens[i - 1].type!=NUMBER&&tokens[i - 1].type!=SNUMBER&&tokens[i - 1].type!= REGISTER&&tokens[i - 1].type !=')'))) {
+ 		if (tokens[i].type=='*'&&(i==0||(tokens[i - 1].type!=NUMBER&&tokens[i - 1].type!=SNUMBER&&tokens[i - 1].type!= REGISTER&&tokens[i-1].type!=MARK&&tokens[i - 1].type !=')'))) {
 			tokens[i].type = POINTER;
 			tokens[i].pri = 6;
 		}
-		if (tokens[i].type =='-'&&(i ==0|| (tokens[i - 1].type != NUMBER && tokens[i - 1].type != SNUMBER && tokens[i - 1].type != REGISTER&&tokens[i - 1].type !=')'))) {
+		if (tokens[i].type =='-'&&(i ==0|| (tokens[i - 1].type != NUMBER && tokens[i - 1].type != SNUMBER && tokens[i - 1].type != REGISTER&&tokens[i-1].type!=MARK&&tokens[i - 1].type !=')'))) {
 			tokens[i].type = MINUS;
 			tokens[i].pri = 6;
  		}
