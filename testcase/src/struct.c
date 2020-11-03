d034ffe9adc3e724ca6d57ca291b1be2a697f3e3
tracer-ics2016 <tracer@njuics.org>
2020-11-03 16:22:10 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:22:10 up 4 days,  2:56,  4 users,  load average: 0.41, 0.19, 0.29 9872d5374e82fbcdeda18f68b73fff4bdd1629f

--- testcase/src/struct.c
+++ testcase/src/struct.c
@@ -17,9 +17,9 @@ int main() {
 		d.pad1[i] = i + 128;
 		d.pad2[i] = i;
 	}
-	set_bp();
+	//set_bp();
 	struct dummy t = fun(d);
-	set_bp();
+	//set_bp();
 	for(i = 0; i < N; i ++) {
 		nemu_assert(t.pad1[i] == i + 128);
 		nemu_assert(t.pad2[i] == i);
