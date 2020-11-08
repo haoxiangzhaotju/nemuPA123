1788ab78013bd7b5893554bc2fe5a874e1800d81
tracer-ics2016 <tracer@njuics.org>
2020-11-08 11:49:42 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:49:42 up 50 min,  5 users,  load average: 0.48, 0.41, 0.29 e7a4a3a6fc4b4dff3fbd1ce673c80ccd3517b13

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -7,9 +7,17 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+typedef struct{
+	swaddr_t prev_ebp;
+	swaddr_t ret_addr;
+	uint32_t args[4];
+
+}PSF;
+
+
 void cpu_exec(uint32_t);
 void display_reg();
-
+void get_func_from_addr(char *tmp,swaddr_t addr);
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
 	static char *line_read = NULL;
@@ -26,6 +34,40 @@ char* rl_gets() {
 	}
 
 	return line_read;
+}
+
+static void read_ebp(swaddr_t addr, PSF *ebp) {
+	ebp->prev_ebp = swaddr_read(addr, 4);
+	ebp->ret_addr = swaddr_read(addr+4, 4);
+	int i;
+	for ( i = 0; i < 4; i++)
+	{
+		ebp->args[i] = swaddr_read(addr+8+4*i, 4);
+	}
+	
+}
+
+static int  cmd_bt(char * args){
+	int a = 0 ;
+	PSF current_ebp;
+	char tmp[32];
+
+	swaddr_t addr = reg_l(R_EBP);
+	current_ebp.ret_addr = cpu.eip;
+	while(addr>0){
+		printf("#%d	0x%08x in\t",a++,current_ebp.ret_addr);
+
+		get_func_from_addr(tmp,current_ebp.ret_addr);
+
+		printf("%s\t",tmp);
+		read_ebp(addr,&current_ebp);
+		if(strcmp(tmp,"main")==0)printf("( )\n");
+		else {
+			printf("( %d, %d, %d, %d )\n", current_ebp.args[0], current_ebp.args[1], current_ebp.args[2], current_ebp.args[3]);
+		}
+		addr = current_ebp.prev_ebp;
+	}
+	return 0;
 }
 
 /* TODO: Add single step */
@@ -146,7 +188,8 @@ static struct {
 	{ "x", "Examine memory", cmd_x },
         { "p", "Evaluate the value of expression", cmd_p },
 	{ "w", "Set watchpoint", cmd_w },
-	{ "d", "Delete watchpoint", cmd_d }
+	{ "d", "Delete watchpoint", cmd_d },
+	{ "bt" , "Print stack frame",cmd_bt},
 
 };
 
