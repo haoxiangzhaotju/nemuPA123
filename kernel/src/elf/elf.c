80c2eb3b93c08c196d7cb9118bc25ec8b4c320b1
tracer-ics2016 <tracer@njuics.org>
2020-12-09 01:14:39 +0000

    >  compile kernel 3019244090 simpleedu Linux simpleedu 4.15.0-126-generic #129-Ubuntu SMP Mon Nov 23 18:53:38 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:14:39 up 17:54,  1 user,  load average: 0.81, 1.17, 0.99 aba9ca7ad98c97332659d6e78ccc70f212d86479

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,6 +2,7 @@
 #include "memory.h"
 #include <string.h>
 #include <elf.h>
+#include <stdio.h>
 
 #define ELF_OFFSET_IN_DISK 0
 
@@ -34,23 +35,27 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-	int i=0;
-	ph = (void*)(buf + elf->e_phoff);
+
 	/* Load each program segment */
-	//panic("please implement me");
-	for(; i<elf->e_phnum; i++,ph++) {
+	// panic("please implement me");
+	int i = 0;
+	ph = (void*)(buf + elf->e_phoff); // goto header table
+	for(; i < elf->e_phnum; i++, ph++ ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 ramdisk_read((void*)ph->p_paddr,ph->p_offset,ph->p_filesz);
+			ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void*)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			
+			
+
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
