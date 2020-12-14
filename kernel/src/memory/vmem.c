fdc462ddae1a947988c3eed1550954609dc5e510
tracer-ics2016 <tracer@njuics.org>
2020-12-14 06:58:53 +0000

    >  compile NEMU 3019244090 simpleedu Linux simpleedu 4.15.0-126-generic #129-Ubuntu SMP Mon Nov 23 18:53:38 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:58:53 up 5 days, 23:38,  1 user,  load average: 0.88, 0.95, 0.98 163440c92ffa80907522d0217c65c032904a20a

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,7 +7,7 @@
 
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
-static PTE vmem_pt[1024] align_to_page;
+PTE vm_pt[NR_PTE] align_to_page;
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
@@ -15,14 +15,14 @@ void create_video_mapping() {
 	 * some page tables to create this mapping.
 	 */
 	//panic("please implement me");
-	PDE *pdp = (PDE *)va_to_pa(get_updir());
-	pdp[0].page_frame = (uint32_t) va_to_pa(vmem_pt) >> 12;
-	pdp[0].present = 1;
+	PDE *pde = (PDE *)va_to_pa(get_updir());
+	
+	pde[0].val = make_pde(va_to_pa(vm_pt));
+	int tot = SCR_SIZE/PAGE_SIZE;
+	if (SCR_SIZE % PAGE_SIZE != 0) tot++;
 	int i;
-	for ( i = VMEM_ADDR / PAGE_SIZE; i < (VMEM_ADDR + SCR_SIZE)/PAGE_SIZE; i++)
-	{
-		vmem_pt[i].page_frame = i;
-		vmem_pt[i].present = 1;
+	for (i = VMEM_ADDR/PAGE_SIZE;i < VMEM_ADDR/PAGE_SIZE + tot;i ++){
+	vm_pt[i].val = make_pte(i<<12);
 	}
 	
 }
