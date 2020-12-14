5d7923d582f2cefb2f3176af7b17d8c54d61d531
tracer-ics2016 <tracer@njuics.org>
2020-12-14 06:44:31 +0000

    >  compile kernel 3019244090 simpleedu Linux simpleedu 4.15.0-126-generic #129-Ubuntu SMP Mon Nov 23 18:53:38 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:44:31 up 5 days, 23:24,  1 user,  load average: 1.98, 1.31, 1.11 f425e0df37a232018a7d7be295262e132fbe79fe

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -70,9 +70,9 @@ uint32_t loader() {
 
 #ifdef IA32_PAGE
 	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
-
+create_video_mapping();
 #ifdef HAS_DEVICE
-	create_video_mapping();
+	
 #endif
 
 	write_cr3(get_ucr3());
